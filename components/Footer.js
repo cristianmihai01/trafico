e7f63f9b209dcb2eecc02fd51d7dc677a59db423
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-09-05 10:10:24 +0300

    added

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,18 @@
-const Footer = () => {
+import Image from 'next/image';
+
+const Footer = ({ footerData }) => {
+  // destructure footer data
+  const { truckImg, hillImg, text, logo, links, form } = footerData;
   return (
-    <footer className='bg-pink-300 bg-footer min-h-[737px] bg-no-repeat bg-left-bottom'>
-      footer
+    <footer className='bg-footer min-h-[737px] bg-no-repeat bg-left-bottom relative'>
+      {/* truck image */}
+      <div className='absolute -top-24  -left-[7%]'>
+        <Image src={truckImg} width={430} height={210} />
+      </div>
+      {/* hill image */}
+      <div className='absolute z-10 top-6'>
+        <Image src={hillImg} width={137} height={92} />
+      </div>
     </footer>
   );
 };
